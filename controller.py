be89e8503087a0c2539df9b43cf4120538161103
Professeur Cot <martin.couture@uqtr.ca>
2025-10-06 21:02:36 -0400

    Amélioration en MVC

--- controller.py
+++ controller.py
@@ -1,22 +1,25 @@
+#Martin Couture
+#Octobre 2025
+#Une classe qui permet de se connecter sur un serveur Traccar via la méthode "login".
+#La méthode "Logout" permet de se déconnecter.
+#Pour recevoir les informations en format JSON de Traccar, utliser la méthode getPositionsJSON.
+#Pour enregistrer les informations de Traccar en format GEOJSON : getPositionsGEOJSON
+
 try:
     __import__("PyQt5")
 except ModuleNotFoundError:
     print(f"Le module 'PyQt5' n'existe pas, veuillez l'installer")
 
 import TraccarAPIGeoJSON as tr2
-import Vue, sys, Controller
+import Vue, sys
 from PyQt5.QtWidgets import QApplication, QCalendarWidget
-
 
 class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
         self.model = tr2.Traccar()
-        self.controller = Controller.Controller(self.model)
-        self.vue = Vue.UI(self.controller)
-        self.controller.setVue(self.vue)
+        self.vue = Vue.UI(self.model)
         self.vue.show()
-
 
 
 if __name__ == "__main__":
